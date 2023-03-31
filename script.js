bf163cea408957849fa0a369f037c38dc8e011f8
Tom Krok <91782249+trak2022z@users.noreply.github.com>
2023-03-31 16:36:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+/**
+ * A program to fetch and display the Astronomy Picture of the Day from the NASA API.
+ */
+"use strict";
+(function(){
+
+  const newDogBtn = document.getElementById("new-dog-btn");
+      const dogImg = document.getElementById("dog-img");
+      
+      // Function to fetch a random dog image
+      function getDogImage() {
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then(response => response.json())
+          .then(data => {
+            // Update the dog image
+            dogImg.src = data.message;
+            dogImg.alt = "Dog Image";
+          })
+          .catch(error => console.error(error));
+      }
+      
+      // Call the function to fetch a random dog image when the page loads
+      getDogImage();
+      
+      // Add an event listener to the "Get New Dog" button to fetch a new image
+      newDogBtn.addEventListener("click", getDogImage);
+
+})();
+
